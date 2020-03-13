ed3df6e39fb5b106504528e452cfa84b2227408e
Nikkey-Liu <1125636300@qq.com>
2020-03-14 01:50:36 +0800

    first step

---
+++ src/main/java/com/atUL/HellowWorld.java
@@ -0,0 +1,12 @@
+package com.atUL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class HellowWorld {
+    public static void main(String[] args){
+        SpringApplication.run(HellowWorld.class,args);
+    }
+}
